f96edb00b3e0d71495fc6438ec5bb017061e593b
MrSakine <msakine20@gmail.com>
2024-04-23 15:51:45 +0000

    promise logic update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,5 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const p0 = signUpUser(firstName, lastName);
   const p1 = uploadPhoto(fileName);
-  return Promise.all([p0, p1]);
+  return Promise.allSettled([p0, p1]).then((res) => res.map((data) => ({ status: data.status, value: data.status.includes('filled') ? data.value : data.reason.toString() })));
 }
