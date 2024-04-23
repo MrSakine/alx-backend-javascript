cf8b9a5dc124320c2d7707071a2d4c2ae9756adf
MrSakine <msakine20@gmail.com>
2024-04-23 15:21:33 +0000

    2-then file updated

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return ({ status: 200, body: 'success' });
-  })
-    .catch(() => {
-      throw new Error('The fake API is not working currently');
-    });
+  promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error('The fake API is not working currently'))
+    .finally(() => console.log('Got a response from the API'));
 }
