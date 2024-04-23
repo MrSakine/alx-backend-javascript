2bc1f64d3ae9a8afc87ac779208e97cebefcd612
MrSakine <msakine20@gmail.com>
2024-04-23 10:47:13 +0000

    Throw an error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const res = mathFunction();
+    queue.push(res);
+  } catch (e) {
+    queue.push(e);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
