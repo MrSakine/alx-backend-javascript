15c03425dfd1b1474d5a6db793a55a671ee474f9
MrSakine <msakine20@gmail.com>
2024-04-25 10:06:07 +0000

    Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // eslint-disable-next-line no-underscore-dangle
+  get code() { return this._code; }
+
+  // eslint-disable-next-line no-underscore-dangle
+  get name() { return this._name; }
+
+  set code(code) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._code = code;
+  }
+
+  set name(name) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    // eslint-disable-next-line no-underscore-dangle
+    return `${this._name} (${this._code})`;
+  }
+}
