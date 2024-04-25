00207b0bd89d31afdcb37175a05887ce81b25698
MrSakine <msakine20@gmail.com>
2024-04-25 11:15:27 +0000

    Hoisting

---
+++ 0x02-ES6_classes/9-hoisting.js
@@ -0,0 +1,54 @@
+export class HolbertonClass {
+  constructor(year, location) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._year = year;
+
+    // eslint-disable-next-line no-underscore-dangle
+    this._location = location;
+  }
+
+  get year() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._year;
+  }
+
+  get location() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._location;
+  }
+}
+
+export class StudentHolberton {
+  // eslint-disable no-underscore-dangle
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
+  get fullName() {
+    // eslint-disable-next-line no-underscore-dangle
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._holbertonClass;
+  }
+
+  get fullStudentDescription() {
+    // eslint-disable-next-line no-underscore-dangle
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
+}
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+export const listOfStudents = [student1, student2, student3, student4, student5];
