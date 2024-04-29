ca677d98c7c26ed89e3058fbee2bc75b0919086f
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-04-29 13:00:21 +0000

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,10 @@
 export default function getListStudents() {
   const firstNames = ['Guillaume', 'James', 'Serena'];
   const locations = ['San Francisco', 'Columbia', 'San Francisco'];
+  const ids = [1, 2, 5];
   const students = Array.of();
   for (let id = 0; id < 3; id += 1) {
-    const obj = { id: (id + 1), firstName: firstNames[id], location: locations[id] };
+    const obj = { id: ids[id], firstName: firstNames[id], location: locations[id] };
     students.push(obj);
   }
   return students;
