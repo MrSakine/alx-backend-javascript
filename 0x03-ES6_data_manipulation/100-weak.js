f38e64797d3d72f031c3cee055221146bf0244f9
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-04-30 11:20:02 +0000

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,7 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const currentCount = weakMap.get(endpoint) || 0;
-  if (currentCount >= 5) {
+  if (currentCount >= 4) {
     throw Error('Endpoint load is high');
   }
   weakMap.set(endpoint, currentCount + 1);
