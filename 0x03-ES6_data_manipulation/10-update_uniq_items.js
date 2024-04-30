b304cb78bd4f10ae5008d3c07fd561dc09a9560c
MrSakine <msakine20@gmail.com>
2024-04-30 11:14:18 +0000

    More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((key, value) => {
+    if (key === 1) {
+      map.set(value, 100);
+    }
+  });
+}
