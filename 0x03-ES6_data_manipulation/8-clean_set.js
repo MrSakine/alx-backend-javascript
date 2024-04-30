b092c80ac9817403341658014903718a0470db78
MrSakine <msakine20@gmail.com>
2024-04-30 10:55:08 +0000

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  let res = '';
+  set.forEach((value) => {
+    if (startString && value.startsWith(startString)) {
+      res += `${value.substring(startString.length)}-`;
+    }
+  });
+  if (res.endsWith('-')) {
+    res = res.slice(0, -1);
+  }
+  return res;
+}
