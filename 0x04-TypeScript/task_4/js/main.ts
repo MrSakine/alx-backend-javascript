2542882fcd83be222eb6efb3e1a6127308ef5212
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-05-01 11:41:24 +0000

    Update main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,1 +1,25 @@
-console.log('App started');
+/// <reference path="Subject.ts" />
+
+import { Subjects } from './js/Subject';
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
+eport const teacher: Subjects.Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+};
+
+cpp.teacher = teacher;
+console.log("C++");
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+java.teacher = teacher;
+console.log("Java");
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+react.teacher = teacher;
+console.log("React");
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
