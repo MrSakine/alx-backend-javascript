956f70de1ad8e985faff8ecce8615a3e0d0ef5b9
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-05-01 10:25:23 +0000

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,22 @@ interface Teacher {
   location: string,
   [propName: string]: any,
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number,
+}
+
+const printTeacher = (firstName: string, lastName: string) : string => `${firstName.charAt(0)}. ${lastName}`;
+
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework = () : string => 'Currently working';
+  displayName = () : string => this.firstName;
+}
