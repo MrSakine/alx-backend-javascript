c9e2966266add7ee031d3afe591739bb13e71ffc
MrSakine <msakine20@gmail.com>
2024-08-19 15:40:06 +0000

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+const displayMessage = require("./0-console");
+
+process.stdin.setEncoding("utf8");
+displayMessage("Welcome to Holberton School, what is your name?");
+process.stdin.on("readable", function () {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write("Your name is: " + chunk);
+  }
+});
+
+process.stdin.on("end", function () {
+  displayMessage("This important software is now closing");
+});
