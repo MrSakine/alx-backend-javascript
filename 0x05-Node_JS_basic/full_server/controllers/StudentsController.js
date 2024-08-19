358368393689158bda9d603daa2a984a0875b141
MrSakine <msakine20@gmail.com>
2024-08-19 22:50:13 +0000

    Organize a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,40 @@
+import { readDatabase } from '../utils.js';
+
+class StudentsController {
+  static async getAllStudents(_, res) {
+    try {
+      const students = await readDatabase(process.argv[2]);
+      let output = 'This is the list of our students\n';
+      const sortedFields = Object.keys(students).sort((a, b) =>
+        a.toLowerCase().localeCompare(b.toLowerCase())
+      );
+      for (const field of sortedFields) {
+        const studentList = students[field].join(', ');
+        output += `Number of students in ${field}: ${students[field].length}. List: ${studentList}\n`;
+      }
+      res.status(200).send(output.trim());
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+
+  static async getAllStudentsByMajor(req, res) {
+    try {
+      const { major } = req.params;
+      if (major !== 'CS' && major !== 'SWE') {
+        return res.status(500).send('Major parameter must be CS or SWE');
+      }
+      const students = await readDatabase(process.argv[2]);
+      if (!students[major]) {
+        return res.status(200).send(`No students found for the major: ${major}`);
+      }
+      const studentList = students[major].join(', ');
+      const output = `List: ${studentList}`;
+      res.status(200).send(output);
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+}
+
+export default StudentsController;
---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,21 @@
+const fs = require('fs').promises;
+
+export async function readDatabase(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    const lines = data.trim().split('\n').splice(1);
+    const fields = {};
+    for (const line of lines) {
+      if (line.trim()) {
+        const [firstname, , , field] = line.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+    }
+    return fields;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
