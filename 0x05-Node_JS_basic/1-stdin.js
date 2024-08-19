2ca531121a65378820d6a830c72102de9d33d8a0
MrSakine <msakine20@gmail.com>
2024-08-19 15:44:40 +0000

    minor changes

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,14 @@
 /**
  * Using Process stdin
  */
-
-const displayMessage = require("./0-console");
-process.stdin.setEncoding("utf8");
-
-displayMessage("Welcome to Holberton School, what is your name?");
-process.stdin.on("readable", function () {
-  var chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write("Your name is: " + chunk);
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.on("readable", () => {
+  const chunk = process.stdin.read();
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
-process.stdin.on("end", function () {
-  displayMessage("This important software is now closing");
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
 });
