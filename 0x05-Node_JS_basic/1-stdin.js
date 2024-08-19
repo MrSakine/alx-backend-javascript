2fec1e6fe08646ffbc649886eb41c1386c81976a
MrSakine <msakine20@gmail.com>
2024-08-19 15:52:46 +0000

    eslint fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-const displayMessage = require("./0-console");
+const displayMessage = require('./0-console');
 
-displayMessage("Welcome to Holberton School, what is your name?");
-process.stdin.on("readable", function () {
-  var chunk = process.stdin.read();
+displayMessage('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write("Your name is: " + chunk);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
-process.stdin.on("end", function () {
-  displayMessage("This important software is now closing");
+process.stdin.on('end', () => {
+  displayMessage('This important software is now closing');
 });
