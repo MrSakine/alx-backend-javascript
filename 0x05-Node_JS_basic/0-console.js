bde1f45f0527c7952e9bdd8c51e244048232f792
MrSakine <msakine20@gmail.com>
2024-08-19 15:31:55 +0000

    added documentation;

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,9 @@
-function displayMessage(value) {
-  process.stdout.write(value + '\n')
-}
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
 module.exports = displayMessage;
