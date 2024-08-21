45699f7f1cfac93c570d8f5eed126a8e5b651259
MrSakine <msakine20@gmail.com>
2024-08-21 12:57:35 +0000

    Basic Integration testing

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,32 @@
+const request = require('request');
+const { expect } = require('chai');
+
+const baseUrl = 'http://localhost:7865';
+
+describe('Index Page', () => {
+  it('should return status code 200', (done) => {
+    request(`${baseUrl}/`, (error, response, _) => {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct message', (done) => {
+    request(`${baseUrl}/`, (error, response, body) => {
+      if (error) return done(error);
+      expect(body).to.equal('Welcome to the payment system');
+      expect(response.headers['content-type']).to.include('text/html');
+      done();
+    });
+  });
+
+  it('should return status code 404', (done) => {
+    request(`${baseUrl}/a`, (error, response, _) => {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(404);
+      expect(response.headers['content-type']).to.include('text/html');
+      done();
+    });
+  });
+});
