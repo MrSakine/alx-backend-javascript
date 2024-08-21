90cf8fc9f728a82164c829f3d1375c1f38233047
MrSakine <msakine20@gmail.com>
2024-08-21 12:29:15 +0000

    Stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleLogSpy;
+  let calculateNumberStub;
+
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+
+  it('should call Utils.calculateNumber with SUM, 100, and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+  });
+
+  it('should log the correct message', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+  });
+});
